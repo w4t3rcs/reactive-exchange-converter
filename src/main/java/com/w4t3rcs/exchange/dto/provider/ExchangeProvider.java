304376ef1c87b96f16f026a1f83a0eb3cea59515
w4t3rcs <uimanovmaks@gmail.com>
2024-04-20 09:41:47 +0200

    Small fix

--- src/main/java/com/w4t3rcs/exchange/dto/provider/ExchangeProvider.java
+++ src/main/java/com/w4t3rcs/exchange/dto/provider/ExchangeProvider.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ExchangeProvider {
     PROVIDER_1("https://api.exchangerate-api.com/v4/latest/%s"),
-    PROVIDER_2("http://api.exchangeratesapi.io/v1/latest?access_key=9abbf9404bc492d41dc78c2abdb410d3&base=%s");
+    PROVIDER_2("https://api.exchangeratesapi.io/v1/latest?access_key=9abbf9404bc492d41dc78c2abdb410d3&base=%s");
 
     private final String uri;
 }
